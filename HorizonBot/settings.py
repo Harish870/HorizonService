b149b90989b8a385ed7505c2e3f6880a62e83711
Harish870 <49521365+Harish870@users.noreply.github.com>
2023-07-29 01:04:29 +0530

    updated databse name

--- HorizonBot/settings.py
+++ HorizonBot/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'HorizonBot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Horizon-ppd',
+        'NAME': 'HorizonPPD',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',
